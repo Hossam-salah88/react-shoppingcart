2c41d21b11e541e314c67d57d63792e6c49388df
Hossam-salah88 <eng.hegazy88@gmail.com>
2022-06-19 18:27:16 +0200

    filter componnet logic

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import { statictext } from "./statictext";
+// import { statictext } from "./statictext";
 import data from "./data.json";
 import Products from "./components/Products/Products";
 import Filter from "./components/Filter/Filter";
 
 function App() {
   const [products, setProducts] = useState(data);
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
+
+  const handelSizeFilter = (e) => {
+    setSize(e.target.value);
+    if (e.target.value === "ALL") {
+      setProducts(data);
+    } else {
+      let productClone = [...products];
+      let newProducts = productClone.filter(
+        (p) => p.sizes.indexOf(e.target.value) !== -1
+      );
+      setProducts(newProducts);
+      console.log(newProducts);
+    }
+  };
+
+  const handelSortFilter = (e) => {
+    let order = e.target.value;
+    setSort(order);
+    let productClone = [...products];
+    let newProducts = productClone.sort(function (a, b) {
+      if (order === "lower") {
+        return a.price - b.price;
+      } else if (order === "highest") {
+        return b.price - a.price;
+      } else if (order === "lastes") {
+        return a.id < b.id ? 1 : -1;
+      }
+    });
+    setProducts(newProducts);
+  };
+
   return (
     <div className="layout">
       <Header />
@@ -15,7 +48,12 @@ function App() {
         <div className="wrapper">
           <Products products={products} />
           <div className="wrapper__filter">
-            <Filter />
+            <Filter
+              handelSizeFilter={handelSizeFilter}
+              handelSortFilter={handelSortFilter}
+              size={size}
+              sort={sort}
+            />
           </div>
         </div>
       </main>
