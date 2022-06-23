6e6c00a93079e087b58baa5dfa02181ba5df8bd1
Hossam-salah88 <eng.hegazy88@gmail.com>
2022-06-24 01:12:35 +0200

    form animation

--- clint/src/components/CheckoutForm/CheckoutForm.jsx
+++ clint/src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "../../css/CheckoutForm/ChechoutForm.css";
+import Slide from "react-reveal/Slide";
 
 const CheckoutForm = (props) => {
   const [value, setValue] = useState("");
@@ -24,41 +25,43 @@ const CheckoutForm = (props) => {
   return (
     <div>
       {" "}
-      <div className="checkout__container">
-        <span
-          onClick={() => props.setShowForm(false)}
-          className="checkout__close"
-        >
-          &times;
-        </span>
-        <form onSubmit={handelFormSubmit} className="checkout__form">
-          <div>
-            <label>Name</label>
-            <input
-              onChange={handelFormChange}
-              type="text"
-              required
-              name="name"
-            />
-          </div>
+      <Slide right>
+        <div className="checkout__container">
+          <span
+            onClick={() => props.setShowForm(false)}
+            className="checkout__close"
+          >
+            &times;
+          </span>
+          <form onSubmit={handelFormSubmit} className="checkout__form">
+            <div>
+              <label>Name</label>
+              <input
+                onChange={handelFormChange}
+                type="text"
+                required
+                name="name"
+              />
+            </div>
 
-          <div>
-            <label>Email</label>
-            <input
-              onChange={handelFormChange}
-              type="email"
-              required
-              name="email"
-            />
-          </div>
+            <div>
+              <label>Email</label>
+              <input
+                onChange={handelFormChange}
+                type="email"
+                required
+                name="email"
+              />
+            </div>
 
-          <div>
-            <button className="cart__btn" type="submit">
-              Checkout
-            </button>
-          </div>
-        </form>
-      </div>
+            <div>
+              <button className="cart__btn" type="submit">
+                Checkout
+              </button>
+            </div>
+          </form>
+        </div>
+      </Slide>
     </div>
   );
 };
