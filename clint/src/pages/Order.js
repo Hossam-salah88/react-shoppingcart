e2e2d49289cd294a231dd30680c850cc4342714f
Hossam-salah88 <eng.hegazy88@gmail.com>
2022-07-04 23:49:32 +0200

    order page redux and style

--- clint/src/pages/Order.js
+++ clint/src/pages/Order.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchOrder } from "../store/actions/order";
+import "./Order.css";
 
-const Order = () => {
-  return <div>Order</div>;
+const Order = (props) => {
+  useEffect(() => {
+    props.fetchOrder();
+  }, []);
+
+  const { order } = props;
+  return (
+    <div className="orders">
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {order &&
+            order.map((p) => (
+              <tr key={p._id}>
+                <td>{p._id}</td>
+                <td>{p.name}</td>
+                <td>{p.email}</td>
+                <td>
+                  {props.cartItems.map((item) => (
+                    <p>
+                      {item.title}, qty: {item.qty}
+                    </p>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default Order;
+export default connect(
+  (state) => {
+    return {
+      order: state.order.orders,
+      cartItems: state.cart.cartItems,
+    };
+  },
+  { fetchOrder }
+)(Order);
